8806e8993871fd4f298266ba263a6439348d1386
KaiUweHella <kai.hella27@gmail.com>
2022-08-30 20:54:55 +0200

    add dialog add user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OptionsComponent } from './options/options.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'options', component: OptionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
